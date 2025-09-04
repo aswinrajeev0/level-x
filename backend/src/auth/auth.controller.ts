97affdf1ce382bd4a531a56343f0612ca2a1f3c5
aswinrajeev0 <aswinrajeev347@gmail.com>
2025-09-04 19:28:27 +0530

    cookie config fixed

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -25,7 +25,7 @@ export class AuthController {
         res.cookie('token', loginResponse.access_token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'lax',
             maxAge: 1000 * 60 * 60 * 24,
         });
 
@@ -54,7 +54,7 @@ export class AuthController {
         res.clearCookie("token", {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'lax',
         });
 
         return { success: true, message: "Logged out successfully" };
